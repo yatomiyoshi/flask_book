a80ca323d17a220190c62643cfc17063718067df
yatomiyoshi <seno.yatomi.0407@gmail.com>
2023-11-17 12:27:02 +0900

    Add new file and complete

---
+++ apps/app.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY="flaskapp123456789",
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY_ECHO=True,
+        WTF_CSRF_SECRET_KEY="flaskapp123456789",
+    )
+    csrf.init_app(app)
+
+    db.init_app(app)
+    Migrate(app, db)
+
+    from apps.crud import views as crud_views
+
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    return app
